11dbc5a158dd9acc96bf3879f2488f638112b8ba
Alexandru Loghin <loghinalex19@gmail.com>
2021-12-02 18:14:32 +0200

    Added login/signup screens

--- src/pages/LogIn.jsx
+++ src/pages/LogIn.jsx
@@ -1,28 +1,39 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./LogIn.css";
 
 function LogIn() {
   return (
-    <div classname="login">
-      <div classname="login-logo">
+    <div className="login">
+      <div className="login-logo">
         <img
           className="navbar-logo"
           src="/smartdrip_logo.png"
           alt="smartdrip_logo"
         ></img>
       </div>
-      <form>
-        <div classname="input">
-          <label for="fname"></label>
-          <input type="text" id="username" name="username"></input>
-        </div>
-        <div classname="input">
-          <label for="pass"></label>
-          <input type="password" id="pass" name="pass"></input>
-        </div>
-        <div classname="input"></div>
-        <input type="submit" value="Log In" />
-      </form>
+      <div className="form-div">
+        <form>
+          <div className="input">
+            <label htmlFor="username">User (email)</label>
+            <input type="text" id="username" name="username"></input>
+          </div>
+          <div className="input">
+            <label htmlFor="pass">Password</label>
+            <input type="password" id="pass" name="pass"></input>
+          </div>
+          <input type="submit" value="Log In" />
+        </form>
+      </div>
+
+      <div className="signup-redirect">
+        <p>
+          <Link to="/signup">
+            Don't have an account? Sign up and order your smart-drip irrigation
+            system here!
+          </Link>
+        </p>
+      </div>
     </div>
   );
 }
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,9 +1,49 @@
 import React from "react";
+import "./SignUp.css";
 
 function SignUp() {
   return (
-    <div>
-      <h1>Signup</h1>
+    <div className="login">
+      <div className="login-logo">
+        <img
+          className="navbar-logo"
+          src="/smartdrip_logo.png"
+          alt="smartdrip_logo"
+        ></img>
+      </div>
+      <div className="form-div">
+        <form>
+          <div className="input">
+            <label htmlFor="fname">First name</label>
+            <input type="text" id="fname" name="fname"></input>
+          </div>
+          <div className="input">
+            <label htmlFor="lname">Last name</label>
+            <input type="text" id="lname" name="lname"></input>
+          </div>
+          <div className="input">
+            <label htmlFor="city">City</label>
+            <input type="text" id="city" name="city"></input>
+          </div>
+          <div className="input">
+            <label htmlFor="address">Address</label>
+            <input type="text" id="address" name="address"></input>
+          </div>
+          <div className="input">
+            <label htmlFor="phonenumber">Phone number</label>
+            <input type="email" id="phonenumber" name="emaphonenumberl"></input>
+          </div>
+          <div className="input">
+            <label htmlFor="email">Email</label>
+            <input type="email" id="email" name="email"></input>
+          </div>
+          <div className="input">
+            <label htmlFor="pass">Password</label>
+            <input type="password" id="pass" name="pass"></input>
+          </div>
+          <input type="submit" value="Sign up and order smart-drip" />
+        </form>
+      </div>
     </div>
   );
 }
