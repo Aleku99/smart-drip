8f78c94bdd69a87f97bc81b71a0ed4db0e833e89
Alexandru Loghin <Alexandru.Loghin2@endava.com>
2022-02-22 12:38:22 +0200

    Implemented chart period change

--- src/components/Charts.jsx
+++ src/components/Charts.jsx
@@ -13,6 +13,7 @@ import {
 import { Line } from 'react-chartjs-2';
 import faker from 'faker';
 import "./Charts.css";
+import {useState} from "react";
 
 
 
@@ -38,35 +39,94 @@ export const options = {
     },
   },
 };
-const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
-export const data = {
-  labels,
-  datasets: [
-    {
-      label: 'Humidity',
-      data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
-      borderColor: 'rgb(53, 162, 235)',
-      backgroundColor: 'rgba(53, 162, 235, 0.5)',
-      color: '#000000'
-    },
-    {
-      label: 'Temperature',
-      data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
-      borderColor: 'rgb(255, 165, 0)',
-      backgroundColor: 'rgba(255, 165, 0, 0.5)',
-      color: '#000000'
-    },
-  ],
-};
+
+// const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+const date = new Date();
+let labels_24hrs = Array.from(Array(24).keys()).reverse();
+labels_24hrs = labels_24hrs.map(element => chooseHour(element));
+let labels_7days = Array.from(Array(7).keys()).reverse();
+labels_7days = labels_7days.map(element => chooseDay(element));
+let labels_1month = Array.from(Array(31).keys()).reverse();
+labels_1month = labels_1month.map(element => chooseDay(element));
+
+function chooseHour(hoursAgo){
+  let date = new Date();
+  let returnedHour = date.getHours() - hoursAgo;
+  if(returnedHour >= 0){
+    return (returnedHour+":00");
+  }
+  else{
+    return (24 + returnedHour).toString() + ":00";
+  }
+}
+function chooseDay(daysAgo){
+  let date = new Date();
+  let daysNumberInPastMonth =  new Date(date.getFullYear(), date.getMonth(), 0).getDate();
+  let returnedDay = date.getDate() - daysAgo;
+
+  if(returnedDay >= 1){
+    return (returnedDay.toString() + "/" + (date.getMonth() + 1).toString());
+  }
+  else{
+    let monthBefore = date.getMonth() - 1;
+    if(date.getMonth() === 0){
+      monthBefore = 11;
+    }
+    return (daysNumberInPastMonth + returnedDay).toString() + "/" + (monthBefore + 1).toString();
+  }
+}
+
 
 function Charts() {
+  chooseDay(7);
+  const [label, setLabel] = useState("24hrs");
+
+  function switchlabels(event){
+    setLabel(event.target.value);
+  }
+
+  function chooseLabel(label){
+    if(label==="24hrs"){
+      return labels_24hrs;
+    }
+    else if(label === "7days"){
+      return labels_7days;
+    }
+    else{
+      return labels_1month;
+    }
+  }
+
+    const data = {
+      labels: chooseLabel(label),
+      datasets: [
+        {
+          label: 'Humidity',
+          data: chooseLabel(label).map(() => faker.datatype.number({ min: -1000, max: 1000 })),
+          borderColor: 'rgb(53, 162, 235)',
+          backgroundColor: 'rgba(53, 162, 235, 0.5)',
+          color: '#000000'
+        },
+        {
+          label: 'Temperature',
+          data: chooseLabel(label).map(() => faker.datatype.number({ min: -1000, max: 1000 })),
+          borderColor: 'rgb(255, 165, 0)',
+          backgroundColor: 'rgba(255, 165, 0, 0.5)',
+          color: '#000000'
+        },
+      ],
+    };
+  
+
+  
+
   return (
     <div className="charts">
       <Line options={options} data={data} />
       <div className="buttons">
-        <button className="button3">24 hours</button>
-        <button className="button3">1 week</button>
-        <button className="button3">1 Month</button>
+        <button className="button3" onClick={switchlabels} value="24hrs">24 hours</button>
+        <button className="button3" onClick={switchlabels} value="7days">1 week</button>
+        <button className="button3" onClick={switchlabels} value="1month">1 Month</button>
       </div>
       
     </div>
