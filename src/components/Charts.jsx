879b3225caf5e601407ea2ba707f673edf8d6492
Alexandru Loghin <loghinalex19@gmail.com>
2022-03-05 18:05:14 +0200

    Finished implementing chart logic

--- src/components/Charts.jsx
+++ src/components/Charts.jsx
@@ -1,5 +1,4 @@
-
-import React from 'react';
+import React from 'react'
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -9,13 +8,10 @@ import {
   Title,
   Tooltip,
   Legend,
-} from 'chart.js';
-import { Line } from 'react-chartjs-2';
-import faker from 'faker';
-import "./Charts.css";
-import {useState, useEffect} from "react";
-
-
+} from 'chart.js'
+import { Line } from 'react-chartjs-2'
+import './Charts.css'
+import { useState, useEffect } from 'react'
 
 ChartJS.register(
   CategoryScale,
@@ -25,137 +21,207 @@ ChartJS.register(
   Title,
   Tooltip,
   Legend
-);
+)
 
 export const options = {
   responsive: true,
   plugins: {
     legend: {
-      position: 'top'
+      position: 'top',
     },
     title: {
       display: true,
       text: 'Humidity & temperature chart',
     },
   },
-};
+}
 
-const date = new Date();
-let labels_24hrs = Array.from(Array(24).keys()).reverse();
-labels_24hrs = labels_24hrs.map(element => chooseHour(element));
-let labels_7days = Array.from(Array(7).keys()).reverse();
-labels_7days = labels_7days.map(element => chooseDay(element));
-let labels_1month = Array.from(Array(31).keys()).reverse();
-labels_1month = labels_1month.map(element => chooseDay(element));
+const date = new Date()
+let labels_24hrs = Array.from(Array(24).keys()).reverse()
+labels_24hrs = labels_24hrs.map((element) => chooseHour(element))
+let labels_7days = Array.from(Array(7).keys()).reverse()
+labels_7days = labels_7days.map((element) => chooseDay(element))
+let labels_1month = Array.from(Array(31).keys()).reverse()
+labels_1month = labels_1month.map((element) => chooseDay(element))
 
-function chooseHour(hoursAgo){
-  let date = new Date();
-  let returnedHour = date.getHours() - hoursAgo;
-  if(returnedHour >= 0){
-    return (returnedHour+":00");
-  }
-  else{
-    return (24 + returnedHour).toString() + ":00";
+function chooseHour(hoursAgo) {
+  let date = new Date()
+  let returnedHour = date.getHours() - hoursAgo
+  if (returnedHour >= 0) {
+    return returnedHour + ':00'
+  } else {
+    return (24 + returnedHour).toString() + ':00'
   }
 }
-function chooseDay(daysAgo){
-  let date = new Date();
-  let daysNumberInPastMonth =  new Date(date.getFullYear(), date.getMonth(), 0).getDate();
-  let returnedDay = date.getDate() - daysAgo;
+function chooseDay(daysAgo) {
+  let date = new Date()
+  let daysNumberInPastMonth = new Date(
+    date.getFullYear(),
+    date.getMonth(),
+    0
+  ).getDate()
+  let returnedDay = date.getDate() - daysAgo
 
-  if(returnedDay >= 1){
-    return (returnedDay.toString() + "/" + (date.getMonth() + 1).toString());
-  }
-  else{
-    let monthBefore = date.getMonth() - 1;
-    if(date.getMonth() === 0){
-      monthBefore = 11;
+  if (returnedDay >= 1) {
+    return returnedDay.toString() + '/' + (date.getMonth() + 1).toString()
+  } else {
+    let monthBefore = date.getMonth() - 1
+    if (date.getMonth() === 0) {
+      monthBefore = 11
     }
-    return (daysNumberInPastMonth + returnedDay).toString() + "/" + (monthBefore + 1).toString();
+    return (
+      (daysNumberInPastMonth + returnedDay).toString() +
+      '/' +
+      (monthBefore + 1).toString()
+    )
   }
 }
-
 
 function Charts(props) {
-  const [label, setLabel] = useState("24hrs");
-  let temperature_data = []; //use in chooseData function
-  let humidity_data = []; //use in chooseData function
-  
-  
-  function switchlabels(event){
-    setLabel(event.target.value);
+  const [label, setLabel] = useState('24hrs')
+  let temperature_data = [] //use in chooseData function
+  let humidity_data = [] //use in chooseData function
+
+  function switchlabels(event) {
+    setLabel(event.target.value)
   }
 
-  function chooseLabel(label){
-    if(label==="24hrs"){
-      return labels_24hrs;
-    }
-    else if(label === "7days"){
-      return labels_7days;
-    }
-    else{
-      return labels_1month;
+  function chooseLabel(label) {
+    if (label === '24hrs') {
+      return labels_24hrs
+    } else if (label === '7days') {
+      return labels_7days
+    } else {
+      return labels_1month
     }
   }
-  function chooseData(label, type){ //continue implementation
-    switch(type){
-      case "HUMIDITY":
-        if(label==="24hrs"){
-          
+  function chooseData(label, type) {
+    switch (type) {
+      case 'HUMIDITY':
+        if (label === '24hrs') {
+          console.log('HUMIDITY24hrs')
+          props.data.humidity_data.reverse().forEach((element, index) => {
+            if (index < 24) {
+              humidity_data.push(element)
+            }
+          })
+        } else if (label === '7days') {
+          console.log('HUMIDITY7days')
+          let average = 0
+          let counter = 0
+          props.data.humidity_data.reverse().forEach((element, index) => {
+            if (counter < 7 && index % 24 === 0 && index !== 0) {
+              average = average / 24
+              humidity_data.push(average)
+              average = element
+              counter++
+            } else {
+              average = average + element
+            }
+          })
+        } else {
+          console.log('HUMIDITY1month')
+          let average = 0
+          let counter = 0
+          props.data.humidity_data.reverse().forEach((element, index) => {
+            if (counter < 32 && index % 24 === 0 && index !== 0) {
+              average = average / 24
+              humidity_data.push(average)
+              average = element
+              counter++
+            } else {
+              average = average + element
+            }
+          })
         }
-        else if(label === "7days"){
-          return labels_7days;
+        break
+      case 'TEMPERATURE':
+        if (label === '24hrs') {
+          console.log('TEMP24hrs')
+          props.data.temperature_data.reverse().forEach((element, index) => {
+            if (index < 24) {
+              temperature_data.push(element)
+            }
+          })
+        } else if (label === '7days') {
+          console.log('TEMP7days')
+          let average = 0
+          let counter = 0
+          props.data.temperature_data.reverse().forEach((element, index) => {
+            if (counter < 7 && index % 24 === 0 && index !== 0) {
+              average = average / 24
+              temperature_data.push(average)
+              average = element
+              counter++
+            } else {
+              average = average + element
+            }
+          })
+        } else {
+          console.log('TEMP1month')
+          let average = 0
+          let counter = 0
+          props.data.temperature_data.reverse().forEach((element, index) => {
+            if (counter < 32 && index % 24 === 0 && index !== 0) {
+              average = average / 24
+              temperature_data.push(average)
+              average = element
+              counter++
+            } else {
+              average = average + element
+            }
+          })
         }
-        else{
-          return labels_1month;
-        }
-        break;
-      case "TEMPERATURE":
-        if(label==="24hrs"){
-          return labels_24hrs;
-        }
-        else if(label === "7days"){
-          return labels_7days;
-        }
-        else{
-          return labels_1month;
-        }
-        break;
+        break
+    }
+
+    if (type === 'HUMIDITY') {
+      humidity_data = humidity_data.reverse()
+      console.log('HUMIDITY ' + humidity_data)
+      return humidity_data
+    } else {
+      temperature_data = temperature_data.reverse()
+      console.log('TEMP ' + temperature_data)
+      return temperature_data
     }
   }
 
-    const data = {
-      labels: chooseLabel(label),
-      datasets: [
-        {
-          label: 'Humidity',
-          data: props.data.humidity_data,
-          borderColor: 'rgb(53, 162, 235)',
-          backgroundColor: 'rgba(53, 162, 235, 0.5)',
-          color: '#000000'
-        },
-        {
-          label: 'Temperature',
-          data: props.data.temperature_data,
-          borderColor: 'rgb(255, 165, 0)',
-          backgroundColor: 'rgba(255, 165, 0, 0.5)',
-          color: '#000000'
-        },
-      ],
-    };
-  
-  
+  const data = {
+    labels: chooseLabel(label),
+    datasets: [
+      {
+        label: 'Humidity',
+        data: chooseData(label, 'HUMIDITY'),
+        borderColor: 'rgb(53, 162, 235)',
+        backgroundColor: 'rgba(53, 162, 235, 0.5)',
+        color: '#000000',
+      },
+      {
+        label: 'Temperature',
+        data: chooseData(label, 'TEMPERATURE'),
+        borderColor: 'rgb(255, 165, 0)',
+        backgroundColor: 'rgba(255, 165, 0, 0.5)',
+        color: '#000000',
+      },
+    ],
+  }
+
   return (
     <div className="charts">
       <Line options={options} data={data} />
       <div className="buttons">
-        <button className="button3" onClick={switchlabels} value="24hrs">24 hours</button>
-        <button className="button3" onClick={switchlabels} value="7days">1 week</button>
-        <button className="button3" onClick={switchlabels} value="1month">1 Month</button>
+        <button className="button3" onClick={switchlabels} value="24hrs">
+          24 hours
+        </button>
+        <button className="button3" onClick={switchlabels} value="7days">
+          1 week
+        </button>
+        <button className="button3" onClick={switchlabels} value="1month">
+          1 Month
+        </button>
       </div>
-      
     </div>
-  );
+  )
 }
 
-export default Charts;
+export default Charts
--- src/pages/CheckHistory.jsx
+++ src/pages/CheckHistory.jsx
@@ -5,22 +5,24 @@ import NavBar from '../components/NavBar'
 import './CheckHistory.css'
 
 function CheckHistory() {
-  const [chartData, setChartData] = useState({humidity_data:[], temperature_data:[]})
+  const [chartData, setChartData] = useState({
+    humidity_data: [],
+    temperature_data: [],
+  })
   useEffect(() => {
     const fetchPrices = async () => {
-      const res = await fetch("http://192.168.100.78:3001/check_history");
-      const data = await res.json();
-      setChartData(data);
-      console.log(1);
-    };
-    fetchPrices();
-  }, []);
+      const res = await fetch('http://192.168.100.78:3001/check_history')
+      const data = await res.json()
+      setChartData(data)
+    }
+    fetchPrices()
+  }, [])
 
   return (
     <div className="check-history">
       <NavBar />
       <div className="check-history-content">
-        <Charts data={chartData}/>
+        <Charts data={chartData} />
       </div>
     </div>
   )
